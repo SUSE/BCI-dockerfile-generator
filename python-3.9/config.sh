c2660990954ffb67427300a2dff14b3c9c494b40
Dan Čermák <dcermak@suse.com>
2022-10-07 14:53:19 +0200

    Initial import

---
+++ python-3.9/config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: (c) 2022 SUSE LLC
+
+set -euo pipefail
+
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+echo "Configure image: [$kiwi_iname]..."
+
+#============================================
+# Import repositories' keys if rpm is present
+#--------------------------------------------
+if command -v rpm > /dev/null; then
+    suseImportBuildKey
+fi
+
+ln -s /usr/bin/pip3.9 /usr/local/bin/pip3; \
+    ln -s /usr/bin/pip3.9 /usr/local/bin/pip; \
+    ln -s /usr/bin/python3.9 /usr/local/bin/python3; \
+    ln -s /usr/bin/pydoc3.9 /usr/local/bin/pydoc
+    
+
+#=======================================
+# Clean up after zypper if it is present
+#---------------------------------------
+if command -v zypper > /dev/null; then
+    zypper -n clean
+fi
+
+rm -rf /var/log/zypp
+
+exit 0
