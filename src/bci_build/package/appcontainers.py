c0ded528af2cc21f86f1a36d116be0dc384fadd5
Alexandre Vicenzi <alexandre.vicenzi@suse.com>
2024-03-22 15:45:46 +0100

    Add MariaDB healthcheck.sh script
    The script can run many tests as well be used in the container
    healthcheck instruction to check health status.
    
    Signed-off-by: Alexandre Vicenzi <alexandre.vicenzi@suse.com>

--- src/bci_build/package/appcontainers.py
+++ src/bci_build/package/appcontainers.py
@@ -130,6 +130,21 @@ HEALTHCHECK --start-period=5m --timeout=5s --interval=5s --retries=2 \
 ]
 
 _MARIADB_ENTRYPOINT = (Path(__file__).parent / "mariadb" / "entrypoint.sh").read_bytes()
+_MARIADB_HEALTHCHECK = (
+    Path(__file__).parent / "mariadb" / "healthcheck.sh"
+).read_bytes()
+_MARIADB_GOSU = b"""#!/bin/bash
+
+u=$1
+shift
+
+if ! id -u $u > /dev/null 2>&1; then
+    echo "Invalid user: $u"
+    exit 1
+fi
+
+setpriv --reuid=$u --regid=$u --clear-groups -- /bin/bash "$@"
+"""
 
 MARIADB_CONTAINERS = []
 MARIADB_CLIENT_CONTAINERS = []
@@ -163,6 +178,8 @@ for os_version in set(ALL_NONBASE_OS_VERSIONS) | {OsVersion.BASALT}:
             entrypoint=["docker-entrypoint.sh"],
             extra_files={
                 "docker-entrypoint.sh": _MARIADB_ENTRYPOINT,
+                "healthcheck.sh": _MARIADB_HEALTHCHECK,
+                "gosu": _MARIADB_GOSU,
                 "_constraints": generate_disk_size_constraints(11),
             },
             support_level=SupportLevel.L3,
@@ -177,7 +194,13 @@ COPY docker-entrypoint.sh /usr/local/bin/
 {DOCKERFILE_RUN} chmod 755 /usr/local/bin/docker-entrypoint.sh
 {DOCKERFILE_RUN} ln -s usr/local/bin/docker-entrypoint.sh / # backwards compat
 
-{DOCKERFILE_RUN} sed -i -e 's,exec gosu mysql ,exec setpriv --reuid=mysql --regid=mysql --clear-groups -- /bin/bash ,g' /usr/local/bin/docker-entrypoint.sh
+# healthcheck from https://github.com/MariaDB/mariadb-docker.git
+COPY healthcheck.sh /usr/local/bin/
+{DOCKERFILE_RUN} chmod 755 /usr/local/bin/healthcheck.sh
+
+COPY gosu /usr/local/bin/gosu
+{DOCKERFILE_RUN} chmod 755 /usr/local/bin/gosu
+
 {DOCKERFILE_RUN} sed -i -e 's,$(pwgen .*),$(openssl rand -base64 36),' /usr/local/bin/docker-entrypoint.sh
 
 # Ensure all logs goes to stdout
