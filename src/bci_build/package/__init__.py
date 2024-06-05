a4b51d39137b56c4e5ad6b8c49c635efdb0922ff
Dirk Müller <dirk@dmllr.de>
2024-06-05 11:14:20 +0200

    Generate the bci-base container via dockerfile generator as well
    Co-Authored-By: Dan Čermák <dcermak@suse.com>

--- src/bci_build/package/__init__.py
+++ src/bci_build/package/__init__.py
@@ -200,6 +200,10 @@ class OsVersion(enum.Enum):
         )
 
     @property
+    def is_ltss(self) -> bool:
+        return self in ALL_OS_LTSS_VERSIONS
+
+    @property
     def is_tumbleweed(self) -> bool:
         return self.value == OsVersion.TUMBLEWEED.value
 
@@ -1005,6 +1009,9 @@ exit 0
 
             return pkg_filter_func
 
+        def pkg_listing_func(pkg: Package) -> str:
+            return f'<package name="{pkg}"/>'
+
         PKG_TYPES = (
             PackageType.DELETE,
             PackageType.BOOTSTRAP,
@@ -1025,8 +1032,7 @@ exit 0
                 res += (
                     f"""  <packages type="{pkg_type}">
     """
-                    + """
-    """.join(f'<package name="{pkg}"/>' for pkg in pkg_list)
+                    + "\n    ".join(pkg_listing_func(pkg) for pkg in pkg_list)
                     + """
   </packages>
 """
@@ -1257,7 +1263,7 @@ exit 0
         the image itself.
 
         """
-        extra_tags = []
+        extra_tags: list[str] = []
         for buildtag in self.build_tags[1:]:
             path, tag = buildtag.split(":")
             if path.endswith(self.name):
@@ -1541,7 +1547,8 @@ class OsContainer(BaseContainerImage):
 
     @property
     def build_tags(self) -> list[str]:
-        tags = []
+        tags: list[str] = []
+
         for name in [self.name] + self.additional_names:
             tags += [
                 f"{self._registry_prefix}/bci-{name}:%OS_VERSION_ID_SP%",
@@ -1588,7 +1595,7 @@ from .appcontainers import REGISTRY_CONTAINERS  # noqa: E402
 from .appcontainers import THREE_EIGHT_NINE_DS_CONTAINERS  # noqa: E402
 from .appcontainers import TOMCAT_CONTAINERS  # noqa: E402
 from .appcontainers import TRIVY_CONTAINERS  # noqa: E402
-from .basalt_base import BASALT_BASE  # noqa: E402
+from .base import BASE_CONTAINERS  # noqa: E402
 from .basecontainers import BUSYBOX_CONTAINERS  # noqa: E402
 from .basecontainers import FIPS_BASE_CONTAINERS  # noqa: E402
 from .basecontainers import GITEA_RUNNER_CONTAINER  # noqa: E402
@@ -1616,7 +1623,7 @@ from .spack import SPACK_CONTAINERS  # noqa: E402
 ALL_CONTAINER_IMAGE_NAMES: dict[str, BaseContainerImage] = {
     f"{bci.uid}-{bci.os_version.pretty_print.lower()}": bci
     for bci in (
-        BASALT_BASE,
+        *BASE_CONTAINERS,
         PYTHON_3_12_CONTAINERS,
         *PYTHON_3_6_CONTAINERS,
         *PYTHON_3_11_CONTAINERS,
