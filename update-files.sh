5f3869e85d420351fd7dd9aad67599385bbdb9dc
Dan Čermák <dcermak@suse.com>
2024-06-21 08:32:06 +0200

    Make shellcheck happy

--- update-files.sh
+++ update-files.sh
@@ -15,9 +15,9 @@ curl -sf -o src/bci_build/package/grafana/run.sh https://raw.githubusercontent.c
 command -v jq > /dev/null || { echo "we need jq installed for update to work"; exit 1; }
 rm -f src/bci_build/package/mariadb/*/entrypoint.sh src/bci_build/package/mariadb/*/healthcheck.sh
 for v in $(jq -r '.mariadb|.[]' src/bci_build/package/package_versions.json | cut -d. -f1-2 | sort -u); do
-    mkdir -p src/bci_build/package/mariadb/$v
-    curl -sf -o src/bci_build/package/mariadb/$v/entrypoint.sh https://raw.githubusercontent.com/MariaDB/mariadb-docker/master/$v/docker-entrypoint.sh
-    curl -sf -o src/bci_build/package/mariadb/$v/healthcheck.sh https://raw.githubusercontent.com/MariaDB/mariadb-docker/master/$v/healthcheck.sh
+    mkdir -p "src/bci_build/package/mariadb/$v"
+    curl -sf -o "src/bci_build/package/mariadb/$v/entrypoint.sh" "https://raw.githubusercontent.com/MariaDB/mariadb-docker/master/$v/docker-entrypoint.sh"
+    curl -sf -o "src/bci_build/package/mariadb/$v/healthcheck.sh" "https://raw.githubusercontent.com/MariaDB/mariadb-docker/master/$v/healthcheck.sh"
 done
 
 curl -sf -o src/bci_build/package/templates/rmt_helm_chart_readme.j2 https://raw.githubusercontent.com/SUSE/helm-charts/main/rmt-helm/README.md
