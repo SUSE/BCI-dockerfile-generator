598220fdf9bf494c3afc3d0862f05a56323f975c
Dan Čermák <dcermak@suse.com>
2024-09-02 14:08:18 +0200

    Enable .Net images for SP7

--- src/dotnet/updater.py
+++ src/dotnet/updater.py
@@ -357,7 +357,7 @@ def _is_latest_dotnet(version: _DOTNET_VERSION_T, os_version: OsVersion) -> bool
 
 DOTNET_IMAGES: list[DotNetBCI] = []
 
-for os_version in (OsVersion.SP6,):
+for os_version in (OsVersion.SP6, OsVersion.SP7):
     for ver in _DOTNET_VERSIONS:
         package_list: list[Package | str] = [
             "dotnet-host",
