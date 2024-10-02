b85b2212619a856ed0964814409b1b31d2eb97d6
Ilyas Shaikh <ilyasgshaikh786@gmail.com>
2024-10-02 19:07:45 +0530

    Added ruff formatting

--- tests/test_util.py
+++ tests/test_util.py
@@ -32,12 +32,13 @@ async def test_ensure_directory_absent(tmp_path: pathlib.Path):
     await ensure_absent(path)
     assert not await aiofiles.os.path.exists(path)
 
+
 @pytest.mark.asyncio
 async def test_ensure_file_absent_when_open_for_reading(tmp_path: pathlib.Path):
     path = tmp_path / "test_file"
     async with aiofiles.open(path, "w") as test_file:
         await test_file.write("foobar")
-    
+
     # Open the file for reading
     async with aiofiles.open(path, "r"):
         pass  # Simply open and close the file
@@ -45,7 +46,8 @@ async def test_ensure_file_absent_when_open_for_reading(tmp_path: pathlib.Path):
     # Ensure the file is removed
     await ensure_absent(path)
     assert not await aiofiles.os.path.exists(path)
-    
+
+
 @pytest.mark.asyncio
 async def test_ensure_non_empty_directory_absent(tmp_path: pathlib.Path):
     # Create a directory
@@ -63,7 +65,8 @@ async def test_ensure_non_empty_directory_absent(tmp_path: pathlib.Path):
 
     # The directory should still exist since it was not removed
     assert await aiofiles.os.path.exists(path)
-    
+
+
 @pytest.mark.asyncio
 async def test_ensure_invalid_path(tmp_path: pathlib.Path):
     # Create an invalid path (e.g., a socket or a broken symlink)
@@ -92,4 +95,3 @@ async def test_ensure_absent_called_on_symlink(tmp_path: pathlib.Path):
 
     # Original file should still exist
     assert await aiofiles.os.path.exists(path)
-
