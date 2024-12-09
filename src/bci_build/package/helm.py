578ba21a250aec8c96679ae8c6dec624757034f1
Dirk Müller <dirk@dmllr.de>
2024-12-09 13:15:13 +0100

    Fix helm version handling
    Ensure we fail the build when the version number is not the expected
    one. use the full version as tag and also set a tag_version.

--- src/bci_build/package/helm.py
+++ src/bci_build/package/helm.py
@@ -7,7 +7,11 @@ from bci_build.os_version import ALL_NONBASE_OS_VERSIONS
 from bci_build.os_version import CAN_BE_LATEST_OS_VERSION
 from bci_build.package import ApplicationStackContainer
 from bci_build.package import Package
+from bci_build.package import ParseVersion
+from bci_build.package import Replacement
 from bci_build.package.helpers import generate_from_image_tag
+from bci_build.package.helpers import generate_package_version_check
+from bci_build.package.versions import format_version
 from bci_build.package.versions import get_pkg_version
 
 HELM_CONTAINERS = [
@@ -17,7 +21,13 @@ HELM_CONTAINERS = [
         from_image=generate_from_image_tag(os_version, "bci-micro"),
         os_version=os_version,
         is_latest=os_version in CAN_BE_LATEST_OS_VERSION,
-        version=get_pkg_version("helm", os_version),
+        version="%%helm_version%%",
+        tag_version=format_version(
+            helm_ver := get_pkg_version("helm", os_version), ParseVersion.MINOR
+        ),
+        config_sh_script=generate_package_version_check(
+            "helm", helm_ver, ParseVersion.MINOR
+        ),
         version_in_uid=False,
         license="Apache-2.0",
         package_list=[
@@ -25,6 +35,13 @@ HELM_CONTAINERS = [
             for name in (
                 "ca-certificates-mozilla",
                 "helm",
+            )
+        ],
+        replacements_via_service=[
+            Replacement(
+                regex_in_build_description="%%helm_version%%",
+                package_name="helm",
+                parse_version=ParseVersion.PATCH,
             )
         ],
         entrypoint=["/usr/bin/helm"],
