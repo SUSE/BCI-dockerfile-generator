4fde5a8add0e0d450e057f8af78fcb3a3c7c02f2
Dirk Müller <dirk@dmllr.de>
2024-12-09 15:08:53 +0100

    Update to build helm as a multistage dockerfile
    This actually makes the package version check work because with
    kiwi we can not run config.sh until it is too late.

--- src/bci_build/package/helm.py
+++ src/bci_build/package/helm.py
@@ -1,12 +1,9 @@
 """BCI Container definition for the helm container."""
 
-from bci_build.container_attributes import BuildType
-from bci_build.container_attributes import PackageType
 from bci_build.container_attributes import SupportLevel
 from bci_build.os_version import ALL_NONBASE_OS_VERSIONS
 from bci_build.os_version import CAN_BE_LATEST_OS_VERSION
 from bci_build.package import ApplicationStackContainer
-from bci_build.package import Package
 from bci_build.package import ParseVersion
 from bci_build.package import Replacement
 from bci_build.package.helpers import generate_from_image_tag
@@ -18,7 +15,6 @@ HELM_CONTAINERS = [
     ApplicationStackContainer(
         name="helm",
         pretty_name="Kubernetes Package Manager",
-        from_image=generate_from_image_tag(os_version, "bci-micro"),
         os_version=os_version,
         is_latest=os_version in CAN_BE_LATEST_OS_VERSION,
         version="%%helm_version%%",
@@ -28,17 +24,15 @@ HELM_CONTAINERS = [
         additional_versions=[
             format_version(helm_ver, ParseVersion.MAJOR),
         ],
-        config_sh_script=generate_package_version_check(
-            "helm", helm_ver, ParseVersion.MINOR
+        from_target_image=generate_from_image_tag(os_version, "bci-micro"),
+        build_stage_custom_end=generate_package_version_check(
+            "helm", helm_ver, ParseVersion.MINOR, use_target=True
         ),
         version_in_uid=False,
         license="Apache-2.0",
         package_list=[
-            Package(name, pkg_type=PackageType.BOOTSTRAP)
-            for name in (
-                "ca-certificates-mozilla",
-                "helm",
-            )
+            "ca-certificates-mozilla",
+            "helm",
         ],
         replacements_via_service=[
             Replacement(
@@ -49,7 +43,6 @@ HELM_CONTAINERS = [
         ],
         entrypoint=["/usr/bin/helm"],
         cmd=["help"],
-        build_recipe_type=BuildType.KIWI,
         support_level=SupportLevel.L3,
     )
     for os_version in ALL_NONBASE_OS_VERSIONS
