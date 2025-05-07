e3557baefdc0313c8a96dd7ad42d89ed78a499f4
Dirk Müller <dirk@dmllr.de>
2025-05-07 18:40:38 +0200

    Define supportlevel and enddate for containerized kiosk

--- src/bci_build/package/kiosk.py
+++ src/bci_build/package/kiosk.py
@@ -1,7 +1,12 @@
+"""Common Definitions for the SUSE containerized Kiosk solution containers."""
+
+import datetime
+
 from bci_build.container_attributes import Arch
 from bci_build.registry import SUSERegistry
 
 KIOSK_EXCLUSIVE_ARCH = [Arch.X86_64, Arch.AARCH64]
+KIOSK_SUPPORT_ENDS = datetime.date(2028, 7, 31)
 
 
 class KioskRegistry(SUSERegistry):
