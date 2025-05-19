251ae2b70c9522f661574050ec70b93e016ae4f3
Dirk Mueller <dirk@dmllr.de>
2025-05-19 17:42:20 +0200

    avoid recursive chown, only create directories
    This requires switching to install(1) so that permissions and
    owners are set correctly.

--- src/bci_build/package/rmt-server/entrypoint.sh
+++ src/bci_build/package/rmt-server/entrypoint.sh
@@ -2,12 +2,11 @@
 set -e
 
 # PV could be empty, make sure the directories exist
-mkdir -p /var/lib/rmt/public/repo
-mkdir -p /var/lib/rmt/public/suma
-mkdir -p /var/lib/rmt/regsharing
-mkdir -p /var/lib/rmt/tmp
-# Set permissions
-chown -R _rmt:nginx /var/lib/rmt
+install -d -m 0755 -o _rmt -g nginx /var/lib/rmt
+install -d -m 0755 -o _rmt -g nginx /var/lib/rmt/public/repo
+install -d -m 0755 -o _rmt -g nginx /var/lib/rmt/public/suma
+install -d -m 0755 -o _rmt -g nginx /var/lib/rmt/regsharing
+install -d -m 0755 -o _rmt -g nginx /var/lib/rmt/tmp
 
 if [ -z "${MYSQL_HOST}" ]; then
 	echo "MYSQL_HOST not set!"
