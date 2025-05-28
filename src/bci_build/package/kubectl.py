70bc7b91e69d4453039e62ecc01dd31625cda135
Dirk Müller <dirk@dmllr.de>
2025-05-28 13:51:20 +0200

    Add kubernetes 1.33

--- src/bci_build/package/kubectl.py
+++ src/bci_build/package/kubectl.py
@@ -10,7 +10,7 @@ from bci_build.package import Replacement
 from bci_build.package.helpers import generate_from_image_tag
 
 _KUBECTL_VERSIONS = {
-    OsVersion.TUMBLEWEED: ("1.30", "1.31", "1.32"),
+    OsVersion.TUMBLEWEED: ("1.30", "1.31", "1.32", "1.33"),
     OsVersion.SP6: ("1.29", "1.31"),
     OsVersion.SP7: ("1.29", "1.31"),
 }
