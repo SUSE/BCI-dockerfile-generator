0af9cd80ea32a656ff0f61a843f6871eb529b720
Dirk Müller <dirk@dmllr.de>
2025-06-30 22:58:16 +0200

    Add postgres for SL 16.0

--- src/bci_build/package/postgres.py
+++ src/bci_build/package/postgres.py
@@ -89,8 +89,14 @@ HEALTHCHECK --interval=10s --start-period=10s --timeout=5s \
     )
     for ver, os_version in (
         [(15, variant) for variant in (OsVersion.TUMBLEWEED,)]
-        + [(16, variant) for variant in (OsVersion.SP7, OsVersion.TUMBLEWEED)]
-        + [(17, variant) for variant in (OsVersion.SP7, OsVersion.TUMBLEWEED)]
+        + [
+            (16, variant)
+            for variant in (OsVersion.SP7, OsVersion.SL16_0, OsVersion.TUMBLEWEED)
+        ]
+        + [
+            (17, variant)
+            for variant in (OsVersion.SP7, OsVersion.SL16_0, OsVersion.TUMBLEWEED)
+        ]
     )
     + [(pg_ver, OsVersion.TUMBLEWEED) for pg_ver in (14, 13)]
 ]
