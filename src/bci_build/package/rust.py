97693cf355ecb60070807801fe55a73217cd0aee
Dirk Müller <dirk@dmllr.de>
2026-02-11 08:44:03 +0100

    Switch stable to rust 1.93 in SLFO

--- src/bci_build/package/rust.py
+++ src/bci_build/package/rust.py
@@ -40,7 +40,7 @@ _RUST_SUPPORT_ENDS = {
 # ensure that the **latest** rust version is the last one!
 _RUST_VERSIONS: list[str] = ["1.92", "1.93"]
 
-_RUST_SL16_VERSIONS: list[str] = ["1.91", "1.92"]
+_RUST_SL16_VERSIONS: list[str] = ["1.92", "1.93"]
 
 _RUST_TW_VERSIONS: list[str] = ["1.92", "1.93"]
 
