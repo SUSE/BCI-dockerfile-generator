52ad4a0a925b4020c4b06a6fb5704321aeee79b3
Dirk Müller <dirk@dmllr.de>
2026-05-27 10:40:32 +0200

    Move cosign to slfo based containers

--- src/bci_build/package/cosign.py
+++ src/bci_build/package/cosign.py
@@ -2,7 +2,7 @@
 
 from bci_build.container_attributes import SupportLevel
 from bci_build.os_version import ALL_NONBASE_OS_VERSIONS
-from bci_build.os_version import CAN_BE_LATEST_OS_VERSION
+from bci_build.os_version import CAN_BE_LATEST_SLFO_OS_VERSION
 from bci_build.package import ApplicationStackContainer
 from bci_build.package import OsVersion
 from bci_build.package.helpers import generate_from_image_tag
@@ -18,7 +18,7 @@ COSIGN_CONTAINERS = [
         pretty_name="cosign",
         custom_description="Signing OCI containers using Sigstore, {based_on_container}.",
         os_version=os_version,
-        is_latest=os_version in CAN_BE_LATEST_OS_VERSION,
+        is_latest=os_version in CAN_BE_LATEST_SLFO_OS_VERSION,
         is_singleton_image=True,
         from_target_image=generate_from_image_tag(os_version, "bci-micro"),
         version="%%cosign_version%%",
