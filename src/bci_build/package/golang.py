68c2566a29069256ef92b89161b8571c3858cc80
Dirk Müller <dirk@dmllr.de>
2026-08-20 10:56:45 +0200

    Go 1.27 also on SLE

--- src/bci_build/package/golang.py
+++ src/bci_build/package/golang.py
@@ -15,7 +15,7 @@ from bci_build.replacement import Replacement
 from bci_build.util import ParseVersion
 
 _GO_VER_T = Literal["1.23", "1.24", "1.25", "1.26", "1.27"]
-_GOLANG_VERSIONS: list[_GO_VER_T] = ["1.25", "1.26"]
+_GOLANG_VERSIONS: list[_GO_VER_T] = ["1.26", "1.27"]
 _GOLANG_TW_VERSIONS: list[_GO_VER_T] = ["1.26", "1.27"]
 _GOLANG_OPENSSL_VERSIONS: list[_GO_VER_T] = ["1.25", "1.26"]
 _GOLANG_VARIANT_T = Literal["", "-openssl"]
